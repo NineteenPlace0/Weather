0c004272e0cd99d75e3237a2694a4820c9f5512a
NineteenPlace0 <b-brockman@live.nl>
2024-06-17 14:24:09 +0200

    add tKinter functionality

--- weather.py
+++ weather.py
@@ -1,10 +1,38 @@
 ### WEATHER ###
+"""V0.1: Setup tKinter"""
 
-"""V0: Initial commit"""
+import tkinter as tk
 
-location = input("What's the weather like in...?")
 
-if location == 'Alaska':
-    print("Cold")
-else:
-    print('Alaska == Cold')
+class Weather:
+    def __init__(self, root):
+        self.root = root
+        self.root.title = "Weather"
+        self.make_widgets()
+
+    def make_widgets(self):
+        self.output = tk.Label(self.root, text="")
+        self.output.pack()
+
+        tk.Label(self.root, text="What's the weather like in...?")
+        self.location = tk.Entry(self.root)
+        self.location.pack()
+
+        self.button = tk.Button(self.root, text="Submit", command=self.check_location)
+        self.button.pack(pady=10)
+
+        self.root.bind('<Return>', lambda event=None: self.check_location())
+
+    def check_location(self):
+        loc = self.location.get()
+
+        if loc == "Alaska":
+            self.output.config(text="Cold.")
+        else:
+            self.output.config(text="It's cold in Alaska.")
+
+
+root = tk.Tk()
+root.geometry("500x400")
+weather = Weather(root)
+root.mainloop()
