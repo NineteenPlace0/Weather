35dd7a8d0c88650fe992e257b7473e157fe6fc85
NineteenPlace0 <b-brockman@live.nl>
2024-06-18 11:39:30 +0200

    add comments for clarity/readability

--- weather.py
+++ weather.py
@@ -1,5 +1,5 @@
 ### WEATHER ###
-"""V0.1: Setup tKinter"""
+"""V0.2: Add comments"""
 
 import tkinter as tk
 
@@ -11,19 +11,25 @@ class Weather:
         self.make_widgets()
 
     def make_widgets(self):
+        """Add functionality to tKinter window."""
+        # Title/Output --- TEST
         self.output = tk.Label(self.root, text="")
         self.output.pack()
 
+        # Input
         tk.Label(self.root, text="What's the weather like in...?")
         self.location = tk.Entry(self.root)
         self.location.pack()
 
+        # Button
         self.button = tk.Button(self.root, text="Submit", command=self.check_location)
         self.button.pack(pady=10)
 
+        # Have 'Enter'-key submit "Input"
         self.root.bind('<Return>', lambda event=None: self.check_location())
 
     def check_location(self):
+        """Check input value."""
         loc = self.location.get()
 
         if loc == "Alaska":
